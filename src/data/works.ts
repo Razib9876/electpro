684a8971a99bb982cf736de6de87b8c52384d59e
Razib987 <mohammadrazib987@gmail.com>
2026-02-25 04:05:07 +0600

    fav title changed and work image fixed

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,28 +1,34 @@
 import type { IWork } from "../types";
+import imageOne from "../assets/image/home-wiring.jpg";
+import imageTwo from "../assets/image/db-board.jpg";
+import imageThree from "../assets/image/load-calculate.jpg";
+import imageFour from "../assets/image/safety.jpg";
+import imageFive from "../assets/image/tools.jpg";
+import imageSix from "../assets/image/govment-compliance.jpg";
 
 export const works: IWork[] = [
   {
     title: "Home Wiring Project",
-    image: "src/assets/image/home-wiring.jpg",
+    image: imageOne,
   },
   {
     title: "Mega DB Board Installation",
-    image: "src/assets/image/db-board.jpg",
+    image: imageTwo,
   },
   {
     title: "Industrial Load Calculation",
-    image: "src/assets/image/load-calculate.jpg",
+    image: imageThree,
   },
   {
     title: "Residential Safety Inspection",
-    image: "src/assets/image/safety.jpg",
+    image: imageFour,
   },
   {
     title: "Electrical Tools Setup",
-    image: "src/assets/image/tools.jpg",
+    image: imageFive,
   },
   {
     title: "Government Compliance Project",
-    image: "src/assets/image/govment-compliance.jpg",
+    image: imageSix,
   },
 ];
